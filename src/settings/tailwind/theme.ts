f659d0bb3890b27dd62260297854f096698475a1
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-19 23:33:40 -0300

    change theme to extend property

--- src/settings/tailwind/theme.ts
+++ src/settings/tailwind/theme.ts
@@ -2,14 +2,14 @@ import type { Config } from 'tailwindcss'
 
 const theme: Pick<Config, 'theme'> = {
     theme: {
-        colors: {
-            primary: '#3BFD4E',
-            secondary: '#2B2B2B',
-            background: '#1E1E1E',
-            text: {
-
-            }
-        },
+        extend: {
+            colors: {
+                primary: '#3BFD4E',
+                secondary: '#2B2B2B',
+                background: '#1E1E1E',
+                light: '#FFFFFF',
+            },
+        }
     }
 }
 
