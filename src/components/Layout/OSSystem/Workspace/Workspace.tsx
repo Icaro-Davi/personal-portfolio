48df537e5990207415575bff13ebb0a25138fa93
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-24 20:51:57 -0300

    WIP: Add workspace to OSSystem

---
+++ src/components/Layout/OSSystem/Workspace/Workspace.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+
+const Workspace: FC = () => {
+    return (
+        <div className="flex flex-1 overflow-hidden">
+            <div>
+                Apps
+            </div>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+
+export default Workspace;
