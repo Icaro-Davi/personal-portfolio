f767d62e1bf547f6037eab8860940c35ae37fdc8
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-26 19:20:22 -0300

    change IconFactory style

--- src/components/Layout/OSSystem/IconFactory.ts
+++ src/components/Layout/OSSystem/IconFactory.ts
@@ -3,29 +3,45 @@ import { MdOutlineSchool } from "react-icons/md";
 import { PiCoffeeBold } from "react-icons/pi";
 
 import type { AppIconsName } from "./types";
+import CreateTailwindStyle from "@/utils/CreateTailwindStyle";
 
-const className = 'w-full h-full p-1 transition-all active:drop-shadow-[0_0_3px_theme(colors.primary)] text-primary';
+const className = {
+    icon: new CreateTailwindStyle<'withGroupHover'>([
+        'w-full',
+        'h-full',
+        'p-1',
+        'transition-all',
+        'text-primary',
+        'active:drop-shadow-[0_0_3px_theme(colors.primary)]',
+    ])
+        .addConditional('withGroupHover', (withGroupHover: boolean) => (
+            withGroupHover
+                ? 'group-hover:drop-shadow-[0_0_3px_theme(colors.primary)]'
+                : ''
+        ))
+}
 
-const IconFactory = (iconName: AppIconsName): React.JSX.Element => {
+const IconFactory = (iconName: AppIconsName, withGroupHover?: boolean): React.JSX.Element => {
     let Icon: JSX.Element | undefined;
+    className.icon.callConditional('withGroupHover', withGroupHover);
     switch (iconName) {
         case 'ABOUT_ME':
-            Icon = RiFolderUserLine({ className });
+            Icon = RiFolderUserLine({ className: className.icon.toClassName() });
             break;
         case 'CONTACT':
-            Icon = RiContactsBookLine({ className });
+            Icon = RiContactsBookLine({ className: className.icon.toClassName() });
             break;
         case 'EDUCATION':
-            Icon = MdOutlineSchool({ className });
+            Icon = MdOutlineSchool({ className: className.icon.toClassName() });
             break;
         case 'PROJECTS':
-            Icon = RiLightbulbLine({ className });
+            Icon = RiLightbulbLine({ className: className.icon.toClassName() });
             break;
         case 'SKILLS':
-            Icon = RiFolder2Line({ className });
+            Icon = RiFolder2Line({ className: className.icon.toClassName() });
             break;
         case 'COFFEE':
-            Icon = PiCoffeeBold({ className });
+            Icon = PiCoffeeBold({ className: className.icon.toClassName() });
             break;
     }
     return Icon;
