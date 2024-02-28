6d749346bff04451c9d64c6380734c1153fea087
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-27 21:04:07 -0300

    move to OSSystem

--- src/components/FileWindow/hooks/useFIleWindowContext/index.ts
+++
@@ -1,5 +0,0 @@
-import { useContext } from "react";
-import FileWindowContext from "./context";
-
-const useFileWindowContext = () => useContext(FileWindowContext);
-export default useFileWindowContext;
--- src/components/FileWindow/hooks/useFileWindowReducer/index.ts
+++
@@ -1,9 +0,0 @@
-import { useReducer } from "react";
-import reducer from "./reducer";
-import INITIAL_STATE from "./initialState";
-
-function useFileWindowReducer(){
-    return useReducer(reducer, INITIAL_STATE);
-}
-
-export default useFileWindowReducer;
