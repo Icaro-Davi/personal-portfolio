9bb8c119f9cf5a8088fefa4ab4aac2ecde0ba4c8
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-27 21:10:56 -0300

    add shortcut icons to workspace

--- src/components/Layout/OSSystem/Workspace/ShortcutAppIconsBar.tsx
+++ src/components/Layout/OSSystem/Workspace/ShortcutAppIconsBar.tsx
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import ShortcutAppWrapper from "./ShortcutAppWrapper";
 import type { FC } from "react";
 
@@ -8,7 +7,10 @@ const ShortcutAppIconsBar: FC = props => {
         <nav className="text-primary">
             <div className="flex flex-col">
                 <ShortcutAppWrapper title="Skills" iconName="SKILLS" />
-                <ShortcutAppWrapper title="Sobre mim teste legals" iconName="ABOUT_ME" />
+                <ShortcutAppWrapper title="Sobre mim" iconName="ABOUT_ME" />
+                <ShortcutAppWrapper title="Contato" iconName="CONTACT" />
+                <ShortcutAppWrapper title="Educação" iconName="EDUCATION" />
+                <ShortcutAppWrapper title="Projetos" iconName="PROJECTS" />
             </div>
         </nav>
     );
