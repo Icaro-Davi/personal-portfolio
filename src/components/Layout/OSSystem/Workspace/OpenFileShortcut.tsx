8763a36f51d0e479d59ac874d34c9a2a0c254384
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-28 22:28:50 -0300

    rename file and add event onClick

---
+++ src/components/Layout/OSSystem/Workspace/OpenFileShortcut.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { memo } from "react";
+import IconFactory from "../IconFactory";
+import className from "./styles";
+import useOSSystemContext from "../hooks/useOSSystemContext";
+
+import type { FC, ReactNode } from "react";
+import type { AppIconsName } from "../types";
+
+const OpenFileShortcut: FC<{
+    title: string;
+    iconName: AppIconsName;
+    openWindowId: string;
+    componentTarget?: ReactNode;
+}> = props => {
+    const { dispatch } = useOSSystemContext();
+
+    const onClickOpenWindow = () => {
+        dispatch({
+            type: 'openWindow',
+            payload: {
+                id: props.openWindowId,
+                title: props.title,
+                iconName: props.iconName,
+                children: props.componentTarget
+            }
+        });
+    }
+
+    return (
+        <a onClick={onClickOpenWindow} className={className.shortcutAppWrapper.toClassName()} title={props.title}>
+            <div className="w-11">
+                {IconFactory(props.iconName, true)}
+            </div>
+            <span className={className.shortcutAppTitle.toClassName()}>
+                {props.title}
+            </span>
+        </a>
+    );
+}
+
+export default memo(OpenFileShortcut);
