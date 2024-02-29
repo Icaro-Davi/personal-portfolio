e9a0c51ab9cc7992dfb54af7213099670156124e
Icaro Davi <icarodaviduarte@gmail.com>
2024-02-28 22:29:50 -0300

    add file to configure appos inside Workspace

--- src/components/Layout/OSSystem/Workspace/ShortcutAppIconsBar.tsx
+++ src/components/Layout/OSSystem/Workspace/ShortcutAppIconsBar.tsx
@@ -1,19 +1,20 @@
-import ShortcutAppWrapper from "./ShortcutAppWrapper";
+import OpenFileShortcut from "./OpenFileShortcut";
+import WorkspaceApps from "../WorkspaceApps";
+
 import type { FC } from "react";
 
-const ShortcutAppIconsBar: FC = props => {
-
-    return (
-        <nav className="text-primary">
-            <div className="flex flex-col">
-                <ShortcutAppWrapper title="Skills" iconName="SKILLS" />
-                <ShortcutAppWrapper title="Sobre mim" iconName="ABOUT_ME" />
-                <ShortcutAppWrapper title="Contato" iconName="CONTACT" />
-                <ShortcutAppWrapper title="Educação" iconName="EDUCATION" />
-                <ShortcutAppWrapper title="Projetos" iconName="PROJECTS" />
-            </div>
-        </nav>
-    );
-}
+const ShortcutAppIconsBar: FC = () => (
+    <nav className="text-primary relative z-10 flex justify-self-start flex-col">
+        {WorkspaceApps.map((app, index) => (
+            <OpenFileShortcut
+                key={`workspace-shortcut-${index}`}
+                title={app.title}
+                iconName={app.iconName}
+                openWindowId={app.id}
+                componentTarget={app.content}
+            />
+        ))}
+    </nav>
+);
 
 export default ShortcutAppIconsBar;
---
+++ src/components/Layout/OSSystem/WorkspaceApps.ts
@@ -0,0 +1,12 @@
+import { WorkspaceAppsType } from "./types";
+
+const WorkspaceApps: WorkspaceAppsType = [
+    {
+        title: 'Habilidades',
+        iconName: 'SKILLS',
+        id: 'skills',
+        content: 'Habilidades'
+    },
+]
+
+export default WorkspaceApps;
