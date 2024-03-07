4578d917d3774eefd7ad3f7792d172b6dcc808c4
Icaro Davi <icarodaviduarte@gmail.com>
2024-03-07 14:58:15 -0300

    add class to remove side effect created by hook

--- src/components/FileWindow/HeaderButtons.tsx
+++ src/components/FileWindow/HeaderButtons.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { FaRegWindowMinimize } from "react-icons/fa";
 import { LuMaximize } from "react-icons/lu";
@@ -14,32 +13,30 @@ const ICON_SIZE = 23;
 const ICON_CLASS_NAME = 'cursor-pointer text-seconday hover:text-light';
 
 const HeaderButtons: FC<HeaderButtonsFileWindowProps> = props => {
-    const [isMaximized, setMaxmized] = useState(false);
-
     return (
-        <div className="flex space-x-2 items-center">
+        <div className="flex space-x-2 items-center window-movement-off">
             <FaRegWindowMinimize
                 onClick={events.headerButtons.onClickMinimize.bind({ ...props })}
                 size={ICON_SIZE}
                 className={ICON_CLASS_NAME}
             />
-            {
-                isMaximized
-                    ? (
-                        <VscChromeRestore
-                            onClick={events.headerButtons.restoreDownWindow.bind({ ...props, setMaxmized })}
-                            size={ICON_SIZE}
-                            className={ICON_CLASS_NAME}
-                        />
-                    )
-                    : (
-                        <LuMaximize
-                            onClick={events.headerButtons.maxmizeWindow.bind({ ...props, setMaxmized })}
-                            size={ICON_SIZE}
-                            className={ICON_CLASS_NAME}
-                        />
-                    )
-            }
+            <div className={ICON_CLASS_NAME}>
+                {
+                    props.isMaximized
+                        ? (
+                            <VscChromeRestore
+                                onClick={events.headerButtons.restoreDownWindow.bind({ ...props })}
+                                size={ICON_SIZE}
+                            />
+                        )
+                        : (
+                            <LuMaximize
+                                onClick={events.headerButtons.maxmizeWindow.bind({ ...props })}
+                                size={ICON_SIZE}
+                            />
+                        )
+                }
+            </div>
             <IoMdClose
                 size={ICON_SIZE + 1}
                 className="cursor-pointer text-secondary hover:text-red-600"
