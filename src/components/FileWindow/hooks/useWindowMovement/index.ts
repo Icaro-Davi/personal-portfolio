86957ad1839e6199e9b7956b5ebeec847849b074
Icaro Davi <icarodaviduarte@gmail.com>
2024-03-07 14:55:48 -0300

    fix side effect by hook

--- src/components/FileWindow/hooks/useWindowMovement/index.ts
+++ src/components/FileWindow/hooks/useWindowMovement/index.ts
@@ -10,23 +10,37 @@ function useWindowMovement<C extends HTMLElement, H extends HTMLElement>(params?
     useEffect(() => {
         let dragAndDrop: DragAndDrop | undefined;
         let resizeWindow: ResizeWindow | undefined;
-
+        const removeSideeffectByClassNameFn = (ev: MouseEvent | TouchEvent) => {
+            ev.stopPropagation();
+        }
         if (containerRef.current && headerRef.current) {
             dragAndDrop = new DragAndDrop({
                 draggableElement: headerRef.current,
                 containerElement: containerRef.current,
                 threshold: params?.headerThreshold,
-                onWindowMovementEnd: params?.onWindowMOvementEnd
+                onWindowMovementEnd: params?.onWindowMovementEnd
             });
             resizeWindow = new ResizeWindow({
                 element: containerRef.current,
-                onWindowMovementEnd: params?.onWindowMOvementEnd
+                onWindowMovementEnd: params?.onWindowMovementEnd
+            });
+            containerRef.current.querySelectorAll('.window-movement-off').forEach((element) => {
+                (element as HTMLElement).addEventListener('mouseup', removeSideeffectByClassNameFn);
+                (element as HTMLElement).addEventListener('mousedown', removeSideeffectByClassNameFn);
+                (element as HTMLElement).addEventListener('mousemove', removeSideeffectByClassNameFn);
+                (element as HTMLElement).addEventListener('touchstart', removeSideeffectByClassNameFn);
             });
         }
 
         return () => {
             dragAndDrop && dragAndDrop.destroy();
             resizeWindow && resizeWindow.destroy();
+            containerRef.current && containerRef.current.querySelectorAll('.window-movement-off').forEach((element) => {
+                (element as HTMLElement).removeEventListener('mouseup', removeSideeffectByClassNameFn);
+                (element as HTMLElement).removeEventListener('mousedown', removeSideeffectByClassNameFn);
+                (element as HTMLElement).removeEventListener('mousemove', removeSideeffectByClassNameFn);
+                (element as HTMLElement).removeEventListener('touchstart', removeSideeffectByClassNameFn);
+            });
         }
     }, [containerRef.current, headerRef.current]);
 
