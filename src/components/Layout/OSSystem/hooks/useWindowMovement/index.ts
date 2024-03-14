cb9ee642e2c838dbecb601bfae4e5a2ab79f15a0
Icaro Davi <icarodaviduarte@gmail.com>
2024-03-14 14:52:09 -0300

    use WindowMovement into useWIndowMovement hook

--- src/components/Layout/OSSystem/hooks/useWindowMovement/index.ts
+++ src/components/Layout/OSSystem/hooks/useWindowMovement/index.ts
@@ -1,46 +1,31 @@
 import { RefObject, useEffect, useRef } from "react";
-import DragAndDrop from "./DragAndDrop";
-import ResizeWindow from "./ResizeWindow";
-import handleSideEffectEventClosure from "./HandleSideEffectClosure";
+import WindowMovement from "./WindowMovement";
 
 import type { useWindowMovmentParams } from "./types";
 
 function useWindowMovement<C extends HTMLElement, H extends HTMLElement>(params?: useWindowMovmentParams): [RefObject<C>, RefObject<H>] {
     const containerRef = useRef<C>(null);
     const headerRef = useRef<H>(null);
-    const dragAndDropRef = useRef<DragAndDrop>();
-    const resizeWindowRef = useRef<ResizeWindow>();
+    const windowMovement = useRef<WindowMovement>();
 
     useEffect(() => {
-
         if (containerRef.current && headerRef.current) {
-            if (!dragAndDropRef.current) {
-                dragAndDropRef.current = new DragAndDrop({
+            if (!windowMovement.current) {
+                windowMovement.current = new WindowMovement({
                     draggableElement: headerRef.current!,
-                    containerElement: containerRef.current!,
-                    threshold: params?.headerThreshold,
-                    onWindowMovementEnd: params?.onWindowMovementEnd
-                });
-            }
-            if (!resizeWindowRef.current) {
-                resizeWindowRef.current = new ResizeWindow({
-                    element: containerRef.current!,
+                    resizableElement: containerRef.current!,
                     onWindowMovementEnd: params?.onWindowMovementEnd
                 });
             }
         }
-        
-        const handlesideEffect = containerRef.current && handleSideEffectEventClosure(containerRef.current, '.window-movement-off');
+
         return () => {
-            dragAndDropRef.current?.destroy();
-            resizeWindowRef.current?.destroy();
-            handlesideEffect?.destroy()
-            dragAndDropRef.current = undefined;
-            resizeWindowRef.current = undefined;
+            windowMovement.current?.destroy();
+            windowMovement.current = undefined;
         };
     }, [containerRef, headerRef]);
 
-    return [containerRef, headerRef];
+return [containerRef, headerRef];
 }
 
 export default useWindowMovement;
