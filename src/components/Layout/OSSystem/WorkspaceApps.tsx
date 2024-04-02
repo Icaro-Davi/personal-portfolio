9d6b199ee9747508df3729f6ae151f354eed2939
Icaro Davi <icarodaviduarte@gmail.com>
2024-04-02 15:17:56 -0300

    change  to .tsx extension

---
+++ src/components/Layout/OSSystem/WorkspaceApps.tsx
@@ -0,0 +1,41 @@
+import AboutMe from "./File/AboutMe";
+
+import { WorkspaceAppsType } from "./types";
+
+const WorkspaceApps: WorkspaceAppsType = [
+    {
+        title: 'Habilidades',
+        iconName: 'SKILLS',
+        id: 'skills',
+        content: 'Habilidades'
+    },
+    {
+        title: 'Sobre mim',
+        fileExtension: '.doc',
+        iconName: 'ABOUT_ME',
+        id: 'about_me',
+        content: <AboutMe />
+    },
+    {
+        title: 'Projetos',
+        iconName: 'PROJECTS',
+        id: 'projects',
+        content: 'projects'
+    },
+    {
+        title: 'Educação',
+        fileExtension: '',
+        iconName: 'EDUCATION',
+        id: 'education',
+        content: 'Educação',
+    },
+    {
+        title: 'Contato',
+        fileExtension: '.exe',
+        iconName: 'CONTACT',
+        id: 'contact',
+        content: 'contato'
+    }
+]
+
+export default WorkspaceApps;
