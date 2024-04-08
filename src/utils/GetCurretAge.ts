9f8a6ed7ee518e92455edc162a691882fe3b6afc
Icaro Davi <icarodaviduarte@gmail.com>
2024-04-08 09:43:24 -0300

    add util function to get current date

---
+++ src/utils/GetCurretAge.ts
@@ -0,0 +1,11 @@
+const getActualAge = (year: number, month: number, day: number) => {
+    const dateNow = new Date();
+    const dateBorn = new Date(year, month, day);
+
+    let age = dateNow.getFullYear() - dateBorn.getFullYear();
+    if (dateNow.getMonth() < dateBorn.getMonth() && dateNow.getDay() < dateBorn.getDay()) age--;
+
+    return age;
+}
+
+export default getActualAge;
