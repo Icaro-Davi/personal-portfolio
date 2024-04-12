b1496a51cac4008dc997b6cf209fe7c61f3e9c81
Icaro Davi <icarodaviduarte@gmail.com>
2024-04-12 00:09:39 -0300

    change params in some functions to accept Set or array of string

--- src/utils/CreateTailwindStyle.ts
+++ src/utils/CreateTailwindStyle.ts
@@ -1,4 +1,4 @@
-type ConditionalReference<T> = { [key in keyof T]: (...args: any) => string };
+type ConditionalReference<T> = { [key in keyof T]: (...args: any) => string | string[] };
 
 class CreateTailwindStyle<T> extends Set {
 
@@ -9,7 +9,7 @@ class CreateTailwindStyle<T> extends Set {
         super(styles);
     }
 
-    public addConditional(key: T, conditionalFunc: (...args: any) => string) {
+    public addConditional(key: T, conditionalFunc: (...args: any) => string | string[]) {
         this.tempStyle = "";
         this.conditional[key as keyof typeof this.conditional] = conditionalFunc;
         return this;
@@ -18,13 +18,21 @@ class CreateTailwindStyle<T> extends Set {
     public callConditional(key: T, ...args: any) {
         const conditional = this.conditional[key as keyof typeof this.conditional];
         if (conditional) {
-            this.tempStyle += ` ${conditional(...args)}`
+            const tailwindRules = conditional(...args);
+            if (Array.isArray(tailwindRules)) {
+                this.tempStyle = Array.from(new Set(tailwindRules)).join(' ');
+            } else {
+                this.tempStyle += ` ${tailwindRules}`
+            }
         }
         return this;
     }
 
-    public toClassName(className: string = "") {
+    public toClassName(className: string | CreateTailwindStyle<any> = "") {
         try {
+            if (className instanceof CreateTailwindStyle) {
+                return `${Array.from(new Set([...Array.from(this), ...Array.from(className)])).join(' ')}`;
+            }
             return `${Array.from(this).join(' ')} ${this.tempStyle} ${className}`.trim().replaceAll(/\s+/g, ' ');
         } finally {
             this.tempStyle = "";
