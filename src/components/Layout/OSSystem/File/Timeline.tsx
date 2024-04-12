bd07e65a08037ec4be5140f9559f821214c16e81
Icaro Davi <icarodaviduarte@gmail.com>
2024-04-12 11:52:55 -0300

    add link to redirect to course certificate

--- src/components/Layout/OSSystem/File/Timeline.tsx
+++ src/components/Layout/OSSystem/File/Timeline.tsx
@@ -12,10 +12,10 @@ const Timeline: FC = () => {
                     description: 'FreeCodeCamp é uma plataforma online que oferece cursos gratuitos de programação, desenvolvimento web e tecnologias relacionadas.',
                     subTitle: 'Cursos finalizados',
                     items: [
-                        { title: 'Back End Development and APIs', sendTo: 'https://google.com' },
-                        { title: 'Front End Development Libraries', sendTo: 'https://google.com' },
-                        { title: 'JavaScript Algorithms and Data Structures', sendTo: 'https://google.com' },
-                        { title: 'Responsive Web Design', sendTo: 'https://google.com' },
+                        { title: 'Back End Development and APIs', sendTo: 'https://www.freecodecamp.org/certification/icaro-davi/back-end-development-and-apis' },
+                        { title: 'Front End Development Libraries', sendTo: 'https://www.freecodecamp.org/certification/icaro-davi/front-end-development-libraries' },
+                        { title: 'JavaScript Algorithms and Data Structures', sendTo: 'https://www.freecodecamp.org/certification/icaro-davi/javascript-algorithms-and-data-structures' },
+                        { title: 'Responsive Web Design', sendTo: 'https://www.freecodecamp.org/certification/icaro-davi/responsive-web-design' },
                     ]
                 },
                 {
@@ -23,8 +23,8 @@ const Timeline: FC = () => {
                     description: 'Udemy é uma plataforma de ensino online que oferece uma ampla variedade de cursos pagos e gratuitos em diversas áreas.',
                     subTitle: 'Cursos finalizados',
                     items: [
-                        { title: 'NGINX Fundamentals: High Performance Servers from Scratch', sendTo: 'https://google.com' },
-                        { title: 'Docker para Desenvolvedores (com Docker Swarm e Kuberneres)', sendTo: 'https://google.com' }
+                        { title: 'NGINX Fundamentals: High Performance Servers from Scratch', sendTo: 'https://www.udemy.com/certificate/UC-81f45c9a-36e4-467e-965d-c824b15f67fb/' },
+                        { title: 'Docker para Desenvolvedores (com Docker Swarm e Kuberneres)', sendTo: 'https://www.udemy.com/certificate/UC-ee492469-c9cf-47ae-91d5-0db955cd5a54/' }
                     ]
                 },
             ]}
