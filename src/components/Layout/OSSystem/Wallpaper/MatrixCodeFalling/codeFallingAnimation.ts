3866054258647f8eb563edfeea488f92fc4f7bae
Icaro Davi <icarodaviduarte@gmail.com>
2024-04-23 21:08:03 -0300

    improve the background falling letters effect

--- src/components/Layout/OSSystem/Wallpaper/MatrixCodeFalling/codeFallingAnimation.ts
+++ src/components/Layout/OSSystem/Wallpaper/MatrixCodeFalling/codeFallingAnimation.ts
@@ -1,34 +1,55 @@
 import { colors } from '@/settings/tailwind/theme';
 import AnimationFrameRate from '../animationFrameRate';
 
+const config = {
+    fallingLetters: '01',
+    letter: {
+        color: colors.primary ?? '#0F0F0F',
+        size: 10
+    },
+    backgroundColor: colors.background ?? '#000000',
+    framesPerSecond: 30
+}
+
 function codeFallingAnimation(canvas: HTMLCanvasElement) {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     const context = canvas.getContext('2d')!;
     if (!context) throw new Error('Failed to get canvas context');
 
-    const letters = '1234567890'.split('');
-    const fontSize = 10;
+    const letters = config.fallingLetters.split('');
+    const fontSize = config.letter.size;
     const columns = Math.round(canvas.width / fontSize);
     const drops = new Array(columns).fill(1);
-    const letterColor = colors.primary ?? '#0F0';
-    const backgroundColor = colors.background + '10' ?? '#00000001'
+    const letterColor = config.letter.color;
+    const backgroundColor = config.backgroundColor;
+    const backgroundColorWithOpacity = `${config.backgroundColor}09`;
 
     function draw() {
-        context.fillStyle = backgroundColor;
+        context.fillStyle = backgroundColorWithOpacity;
         context.fillRect(0, 0, canvas.width, canvas.height);
         drops.forEach((drop, index) => {
             let letter = letters[Math.floor(Math.random() * letters.length)];
+            
+            const x = index * fontSize;
+            const y = drop * fontSize;
+            
+            context.fillStyle = backgroundColor;
+            context.fillRect(x, y, fontSize, fontSize);
+            
             context.fillStyle = letterColor;
-            context.fillText(letter, index * fontSize, drops[index] * fontSize);
-            drops[index]++;
-            if (drops[index] * fontSize > canvas.height && Math.random() > .95) {
+            context.fillText(letter, x, y);
+            
+            drop++;
+            if (drop * fontSize > canvas.height && Math.random() > .95) {
                 drops[index] = 0;
+            } else {
+                drops[index] = drop;
             }
         });
     }
 
-    const animationFrameRate = new AnimationFrameRate(30)
+    const animationFrameRate = new AnimationFrameRate(config.framesPerSecond)
         .registerAnimation(draw)
         .startTime();
 
