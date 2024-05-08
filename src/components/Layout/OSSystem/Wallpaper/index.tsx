d042eea8eb17fd1ee41b3500d4e40cb1d6890c78
Icaro Davi <icarodaviduarte@gmail.com>
2024-05-08 14:47:10 -0300

    handle wallpaper visibility

--- src/components/Layout/OSSystem/Wallpaper/index.tsx
+++ src/components/Layout/OSSystem/Wallpaper/index.tsx
@@ -1,10 +1,19 @@
-import { FC } from "react";
+import { PiCoffeeBold } from "react-icons/pi";
 import MatrixCodeFalling from "./MatrixCodeFalling";
 
-const Wallpaper: FC = props => (
-    <div className="w-full h-full absolute left-0 top-0">
-        <MatrixCodeFalling />
-    </div>
-);
+import type { FC } from "react";
+import useOSSystemContext from "../hooks/useOSSystemContext";
+
+const Wallpaper: FC = props => {
+    const { state } = useOSSystemContext();
+    return (
+        <div className="w-full h-full absolute left-0 top-0">
+            <div className="absolute w-full h-full flex justify-center items-center">
+                <PiCoffeeBold className="text-background opacity-80" size={400} />
+            </div>
+            <MatrixCodeFalling animationActive={state.wallpaper.isActive} />
+        </div>
+    );
+}
 
 export default Wallpaper;
