72ce86bf30aeb5b3a01cbf7f9df4dbd2a87017e0
Icaro Davi <icarodaviduarte@gmail.com>
2024-05-13 16:50:02 -0300

    add metadata to page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,5 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import className from "./styles";
-
-export const metadata: Metadata = {
-  title: "Icaro Davi Dev",
-  description: "Olá meu nome é Icaro Davi e sou um desenvolvedor",
-};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,22 @@
 import OSSystem from "@/components/Layout/OSSystem";
+import type { Metadata } from "next";
+
+const title = "Icaro Davi Dev | Portfólio";
+const description = "Olá meu nome é Icaro Davi e esté é o meu portifólio, entre e descubra mais sobre mim.";
+export const metadata: Metadata = {
+  title, description,
+  creator: 'Icaro Davi Duarte Romualdo',
+  keywords: ['Portfolio', 'Jobs', 'FullStack', 'FrontEnd', 'BackEnd', 'Developer'],
+  metadataBase: new URL(process.env.ABSOLUTE_PATH_URL ?? ''),
+  openGraph: {
+    title, description,
+    locale: 'pt_BR',
+    type: 'website',
+    images: '/coffee_os.png',
+    url: '/',
+    siteName: 'Icaro Davi Dev'
+  }
+};
 
 export default function Main() {
   return (
