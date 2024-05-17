a8de06edc599cc7e6d7004f40363358cc2112398
Icaro Davi <icarodaviduarte@gmail.com>
2024-05-17 16:37:19 -0300

    add tags

--- src/components/Card/Project/index.tsx
+++ src/components/Card/Project/index.tsx
@@ -3,24 +3,27 @@ import Image from 'next/image';
 import { IoMdOpen, IoMdCode } from "react-icons/io";
 import className from "./styled";
 import LinkButton from "./LinkButton";
+import TagDashed from "@/components/Tag/Dashed";
 
 import type { FC } from "react";
+import type { KnownTechnologyRefType } from "@/utils/KnownTechnologyRefs";
 
 type ProjectCardProps = {
     title: string;
     imgSrc: string;
     codeUrl?: string;
     projectUrl?: string;
+    tags?: KnownTechnologyRefType[];
 }
 
 const ProjectCard: FC<ProjectCardProps> = props => (
     <div className={className.container.toClassName()}>
         <div className="border-2 border-primary">
-            <div className="flex flex-col w-52 h-64 text-white relative z-10 bg-background">
-                <h2 className="text-center text-b py-1 px-2 font-bold">
+            <div className="flex flex-col w-52 min-h-64 px-2 text-white relative z-10 bg-background">
+                <h2 className="text-center text-b py-1 font-bold">
                     {props.title}
                 </h2>
-                <div className="flex-1 border-2 border-dashed inline-block mx-2 relative">
+                <div className="border-2 border-dashed inline-block relative flex-1 min-h-32">
                     <Image
                         title={props.title}
                         alt={props.title}
@@ -29,7 +32,21 @@ const ProjectCard: FC<ProjectCardProps> = props => (
                         fill={true}
                     />
                 </div>
-                <div className="flex gap-2 px-2 py-1">
+                {props.tags?.length && (
+                    <div className="flex flex-wrap justify-center py-1 gap-1 ">
+                        {props.tags.map((tag, i) => (
+                            <TagDashed
+                                key={`tag-dashed-${tag.name.toLocaleLowerCase()}-${i}`}
+                                className="border-secondary"
+                                href={tag.href}
+                                icon={tag.icon}
+                            >
+                                {tag.name}
+                            </TagDashed>
+                        ))}
+                    </div>
+                )}
+                <div className="flex gap-2 py-1">
                     <LinkButton href={props.codeUrl} icon={IoMdCode}>
                         Código
                     </LinkButton>
