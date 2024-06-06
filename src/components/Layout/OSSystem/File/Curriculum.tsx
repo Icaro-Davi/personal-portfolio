400fc26d5bc24969dd2df215e9c3214cdc2f67aa
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-06 15:52:51 -0300

    feat: add link to download PDF from link

--- src/components/Layout/OSSystem/File/Curriculum.tsx
+++ src/components/Layout/OSSystem/File/Curriculum.tsx
@@ -1,4 +1,7 @@
-import { useState, type FC } from "react";
+import { useState } from "react";
+import UnderlineGlitchLinkButton from "@/components/Buttons/UnderlineLinkButton";
+
+import type { FC } from "react";
 
 const Curriculum: FC = () => {
     const [isFocused, setFocus] = useState(false);
@@ -16,7 +19,14 @@ const Curriculum: FC = () => {
                 type="application/pdf"
                 className="w-full h-full"
             >
-                <div>No online PDF viewer installed</div>
+                <div className="text-white text-center p-1">
+                    <p className="">
+                        Você não possui o plugin necessário para poder visualizar o PDF, não se preocupe você pode fazer o
+                        <UnderlineGlitchLinkButton href="/FullStack.pdf" className="pl-1">
+                            download do PDF clicando aqui.
+                        </UnderlineGlitchLinkButton>
+                    </p>
+                </div>
             </object>
         </div>
     );
