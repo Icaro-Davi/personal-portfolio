d8797320581e5a8926a0e1e17beaf941c73b95d8
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-13 16:41:34 -0300

    change: add localized translation

--- src/components/Layout/OSSystem/File/AboutMe.tsx
+++ src/components/Layout/OSSystem/File/AboutMe.tsx
@@ -1,44 +1,47 @@
 import Image from "next/image";
 import { memo } from "react";
+import { useTranslations } from "next-intl";
 import NerdFaceEmoji from "@/components/Sticks/NerdFace";
 import DocumentFile from "@/components/Files/Document";
 
 import type { FC } from "react";
+import i18nRichElements from "@/utils/i18n/richElements";
 
-const AboutMe: FC = () => (
+const AboutMe: FC = () => {
+  const t = useTranslations("Index.file.about_me");
+  const ownerName = 'Icaro Davi Duarte Romualdo';
+  return (
     <DocumentFile>
-        <h2 className="font-bold text-center text-3xl flex flex-wrap justify-center space-x-1 pb-2">
-            <span className="flex items-center leading-none">Olá me chamo Icaro Davi Duarte Romualdo</span>
-            <NerdFaceEmoji size={36} />
-        </h2>
-        <div className="pt-4 flex flex-wrap justify-center items-center text-lg">
-            <div className="border w-[230px] h-[320px] mr-2 relative">
-                <Image
-                    title="Icaro Davi"
-                    alt="Foto do Icaro Davi"
-                    src="/icaro_picture.jpg"
-                    className="w-full h-full object-cover"
-                    fill={true}
-                />
-            </div>
-            <div className="flex-1 min-w-[300px]">
-                <p className="font-medium pt-4 text-justify">
-                    Sou desenvolvedor <strong>FullStack</strong> apaixonado por tecnologia, com experiência sólida em projetos web. Graduei-me em Sistemas de Informações em 2020
-                    e desde então venho me aperfeiçoando continuamente.
-                </p>
-                <p className="font-medium pt-4 text-justify">
-                    Meus principais conhecimentos estão na criação de aplicações web utilizando <strong>HTML</strong>, <strong>CSS3</strong>, <strong>Javascript/Typescript</strong> e <strong>NodeJS</strong>. Tenho interesse em adquirir novos
-                    conhecimentos, principalmente sobre tecnologias, e buscar manter-me atualizado sobre as últimas tendências.
-                </p>
-            </div>
-            <p className="font-medium pt-4 text-justify min-w-[300px]">
-                Contando um pouco sobre mim, sou <strong>autodidata</strong> e, em meus momentos livres, gosto de estudar temas relacionados às tecnologias de programação. 
-                Para me divertir e manter a saúde em dia, faço academia, e também consumo animes e mangás. Se precisasse me definir, 
-                diria que sou uma pessoa <strong>concentrada</strong>, <strong>focada</strong>, <strong>fiel</strong> e <strong>companheira</strong>, sempre em busca de desafios que me permitam crescer 
-                profissionalmente.
-            </p>
+      <h2 className="font-bold text-center text-3xl flex flex-wrap justify-center space-x-1 pb-2">
+        <span className="flex items-center leading-none">
+          {t('document_file.title', { ownerName })}
+        </span>
+        <NerdFaceEmoji size={36} />
+      </h2>
+      <div className="pt-4 flex flex-wrap justify-center items-center text-lg">
+        <div className="border w-[230px] h-[320px] mr-2 relative">
+          <Image
+            title={t('document_file.owner_picture.title', { ownerName })}
+            alt={t('document_file.owner_picture.alt', { ownerName })}
+            src="/icaro_picture.jpg"
+            className="w-full h-full object-cover"
+            fill={true}
+          />
         </div>
+        <div className="flex-1 min-w-[300px]">
+          <p className="font-medium pt-4 text-justify">
+            {t.rich('document_file.paragraphs.1st', i18nRichElements)}
+          </p>
+          <p className="font-medium pt-4 text-justify">
+            {t.rich('document_file.paragraphs.2st', i18nRichElements)}
+          </p>
+        </div>
+        <p className="font-medium pt-4 text-justify">
+          {t.rich('document_file.paragraphs.3st', i18nRichElements)}
+        </p>
+      </div>
     </DocumentFile>
-);
+  );
+};
 
 export default memo(AboutMe);
