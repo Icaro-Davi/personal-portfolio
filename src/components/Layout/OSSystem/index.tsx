fc93b1634fadb4da04170022e90e4fde748d0dc9
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-12 22:48:27 -0300

    change: remove React.suspense

--- src/components/Layout/OSSystem/index.tsx
+++ src/components/Layout/OSSystem/index.tsx
@@ -1,22 +1,22 @@
-'use client';
+"use client";
 
-import { Suspense } from "react";
 import dynamic from "next/dynamic";
 import OSSystemProvider from "./hooks/useOSSystemContext/Provider";
 
-const Screen = dynamic(() => import("./Screen"), { ssr: false, loading: () => <Loading /> });
+const Screen = dynamic(() => import("./Screen"), {
+  ssr: false,
+  loading: () => <Loading />,
+});
 
 import type { FC } from "react";
 import Loading from "./loading";
 
 const OSSystem: FC = () => {
-    return (
-        <OSSystemProvider>
-            <Suspense fallback={<Loading />}>
-                <Screen />
-            </Suspense>
-        </OSSystemProvider>
-    );
-}
+  return (
+    <OSSystemProvider>
+      <Screen />
+    </OSSystemProvider>
+  );
+};
 
 export default OSSystem;
