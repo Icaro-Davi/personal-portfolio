1fc79ea539316772f64d41db34836de5e0fba518
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-13 09:18:51 -0300

    change: translate title

--- src/components/Layout/OSSystem/Workspace/ShortcutAppIconsBar.tsx
+++ src/components/Layout/OSSystem/Workspace/ShortcutAppIconsBar.tsx
@@ -2,20 +2,24 @@ import OpenFileShortcut from "./OpenFileShortcut";
 import WorkspaceApps from "../WorkspaceApps";
 
 import type { FC } from "react";
+import { useTranslations } from "next-intl";
 
-const ShortcutAppIconsBar: FC = () => (
+const ShortcutAppIconsBar: FC = () => {
+  const t = useTranslations("Index.file");
+  return (
     <nav className="text-primary relative z-10 p-2 flex justify-self-start flex-col flex-wrap">
-        {WorkspaceApps.map((app, index) => (
-            <OpenFileShortcut
-                key={`workspace-shortcut-${index}`}
-                title={app.title}
-                fileExtension={app.fileExtension}
-                iconName={app.iconName}
-                openWindowId={app.id}
-                componentTarget={app.content}
-            />
-        ))}
+      {WorkspaceApps.map((app, index) => (
+        <OpenFileShortcut
+          key={`workspace-shortcut-${index}`}
+          title={t(`${app.id}.title` as any)}
+          fileExtension={app.fileExtension}
+          iconName={app.iconName}
+          openWindowId={app.id}
+          componentTarget={app.content}
+        />
+      ))}
     </nav>
-);
+  );
+};
 
 export default ShortcutAppIconsBar;
