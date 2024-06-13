12800d1d025f7bc41f320d52e850e08e48abe70d
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-13 15:26:36 -0300

    change: translate curriculum

--- src/components/Layout/OSSystem/File/Curriculum.tsx
+++ src/components/Layout/OSSystem/File/Curriculum.tsx
@@ -1,16 +1,21 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
+import { useTranslations } from "next-intl";
 import UnderlineGlitchLinkButton from "@/components/Buttons/UnderlineLinkButton";
+import { ClientCookie } from "@/utils/cookies";
 
 import type { FC } from "react";
 
 const Curriculum: FC = () => {
+  const DEFAULT_PDF_URL = `/FullStack_pt.pdf`;
+  const PDF_URL = `/FullStack_${ClientCookie.getLocale()}.pdf`;
   const [barrierVisibility, setBarrierVisibility] = useState(false);
   const debounceRef = useRef<NodeJS.Timeout>();
+  const t = useTranslations("Index.file.curriculum");
 
   const showBarrier = () => {
     clearTimeout(debounceRef.current);
     setBarrierVisibility(true);
-  }
+  };
   const hideBarrier = () => {
     clearTimeout(debounceRef.current);
     debounceRef.current = setTimeout(setBarrierVisibility, 100, false);
@@ -18,22 +23,25 @@ const Curriculum: FC = () => {
 
   return (
     <div
-      className={`h-full relative ${barrierVisibility ? 'select-none' : ''}`}
+      className={`h-full relative ${barrierVisibility ? "select-none" : ""}`}
       onMouseEnter={hideBarrier}
       onMouseLeave={showBarrier}
     >
       {barrierVisibility && <div className="absolute w-full h-full" />}
       <object
-        data="/FullStack.pdf"
+        data={PDF_URL}
         type="application/pdf"
         className="w-full h-full"
+        onErrorCapture={(e) => {
+          if (e.type !== "error") return;
+          (e.target as HTMLObjectElement).data = DEFAULT_PDF_URL;
+        }}
       >
         <div className="text-white text-center p-1">
-          <p className="">
-            Você não possui o plugin necessário para poder visualizar o PDF, não
-            se preocupe você pode fazer o
-            <UnderlineGlitchLinkButton href="/FullStack.pdf" className="pl-1">
-              download do PDF clicando aqui.
+          <p>
+            {t("failed_preview_PDF.message")}
+            <UnderlineGlitchLinkButton href={PDF_URL} className="pl-1">
+              {t("failed_preview_PDF.click_here_download_button")}.
             </UnderlineGlitchLinkButton>
           </p>
         </div>
