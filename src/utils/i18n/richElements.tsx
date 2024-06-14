2a255456135c57bb6b2597c39e4828937c61ce71
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-14 00:06:33 -0300

    change: add new element to RichElements

--- src/utils/i18n/richElements.tsx
+++ src/utils/i18n/richElements.tsx
@@ -1,7 +1,9 @@
+import UnderlineGlitchLinkButton from "@/components/Buttons/UnderlineLinkButton";
 import type { RichTranslationValues } from "next-intl";
 
 const i18nRichElements: RichTranslationValues = {
     b: (chunk) => <b>{chunk}</b>,
+    TextLinkMail: (chunk) => <UnderlineGlitchLinkButton href="mailto:icarodaviduarte@gmail.com">{chunk}</UnderlineGlitchLinkButton> 
 }
 
 export default i18nRichElements;
