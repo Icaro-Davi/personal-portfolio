2ec93faf407d580f9ae8b323d5a63d2a8c69203d
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-18 12:58:24 -0300

    change: add default props to use in span component

--- src/components/Glitch/Text.tsx
+++ src/components/Glitch/Text.tsx
@@ -3,72 +3,80 @@ import CreateTailwindStyle from "@/utils/CreateTailwindStyle";
 import type { FC, ReactNode } from "react";
 
 const className = {
-    text: new CreateTailwindStyle([
-        '[--offset-1:2px]',
-        '[--offset-2:-2px]',
-        '[--highlight-1:#FD2F2F]',
-        '[--highlight-2:#3636FF]',
-        'inline-block',
+  text: new CreateTailwindStyle([
+    "[--offset-1:2px]",
+    "[--offset-2:-2px]",
+    "[--highlight-1:#FD2F2F]",
+    "[--highlight-2:#3636FF]",
+    "inline-block",
 
-        'before:content-[attr(data-text)]',
-        'before:absolute',
-        'before:top-0',
-        'before:left-0',
-        'before:w-full',
-        'before:h-full',
+    "before:content-[attr(data-text)]",
+    "before:absolute",
+    "before:top-0",
+    "before:left-0",
+    "before:w-full",
+    "before:h-full",
 
-        'after:content-[attr(data-text)]',
-        'after:absolute',
-        'after:top-0',
-        'after:left-0',
-        'after:w-full',
-        'after:h-full',
+    "after:content-[attr(data-text)]",
+    "after:absolute",
+    "after:top-0",
+    "after:left-0",
+    "after:w-full",
+    "after:h-full",
 
-        'before:left-[var(--offset-1)]',
-        'before:[text-shadow:-2px_0_var(--highlight-1)]',
-        'before:[clip:rect(24px,550px,90px,0)]',
-        'before:[animation:glitchEffectOne_var(--effect-time-1)_infinite_linear_alternate-reverse]',
+    "before:left-[var(--offset-1)]",
+    "before:[text-shadow:-2px_0_var(--highlight-1)]",
+    "before:[clip:rect(24px,550px,90px,0)]",
+    "before:[animation:glitchEffectOne_var(--effect-time-1)_infinite_linear_alternate-reverse]",
 
-        'after:left-[var(--offset-2)]',
-        'after:[text-shadow:-2px_0_var(--highlight-1)]',
-        'after:[clip:rect(85px,550px,140px,0)]',
-        'after:[animation:glitchEffectTwo_var(--effect-time-2)_infinite_linear_alternate-reverse]',
-    ])
-}
+    "after:left-[var(--offset-2)]",
+    "after:[text-shadow:-2px_0_var(--highlight-1)]",
+    "after:[clip:rect(85px,550px,140px,0)]",
+    "after:[animation:glitchEffectTwo_var(--effect-time-2)_infinite_linear_alternate-reverse]",
+  ]),
+};
 
 const extractTextFromChild = (children: ReactNode) => {
-    let text = '';
-    Children.forEach(children, child => {
-        if (typeof child === 'string') {
-            text += child;
-            return;
-        }
-        if (isValidElement(child)) {
-            text += extractTextFromChild(child.props.children);
-        }
-    });
-    return text;
-}
+  let text = "";
+  Children.forEach(children, (child) => {
+    if (typeof child === "string") {
+      text += child;
+      return;
+    }
+    if (isValidElement(child)) {
+      text += extractTextFromChild(child.props.children);
+    }
+  });
+  return text;
+};
 
-const GlitchText: FC<{ children?: ReactNode }> = props => {
-    const childText = extractTextFromChild(props.children);
-    if (typeof childText !== 'string') throw new Error('Children must be string');
-    return (
-        <span
-            style={{
-                position: 'relative',
-                ...{
-                    '--effect-time-1': `${Math.floor(Math.random() * 10) + 3}s`,
-                    '--effect-time-2': `${Math.floor(Math.random() * 10) + 1}s`,
-                }
-            }}
-            className={className.text.toClassName()}
-            data-text={childText}
-        >
-            {props.children}
-        </span>
-    )
-}
+type GlitchTextProps = { children?: ReactNode } & Omit<
+  React.HTMLAttributes<HTMLSpanElement>,
+  "style"
+>;
+
+const GlitchText: FC<GlitchTextProps> = ({ className: CN, children, ...props }) => {
+  const childText = extractTextFromChild(children);
+
+  if (typeof childText !== "string") throw new Error("Children must be string");
+
+  return (
+    <span
+      style={{
+        position: "relative",
+        ...{
+          "--effect-time-1": `${Math.floor(Math.random() * 10) + 3}s`,
+          "--effect-time-2": `${Math.floor(Math.random() * 10) + 1}s`,
+        },
+      }}
+      className={className.text.toClassName(CN)}
+      data-text={childText}
+      {...props}
+    >
+      {children}
+    </span>
+  );
+};
 
 export default memo(GlitchText);
 
