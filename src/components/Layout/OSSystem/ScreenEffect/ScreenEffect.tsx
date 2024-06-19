fc2c87a1854ed8070e47a870e20537744b67c968
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-19 17:55:01 -0300

    feat: add screen effect to works in server side

---
+++ src/components/Layout/OSSystem/ScreenEffect/ScreenEffect.tsx
@@ -0,0 +1,9 @@
+import OldScreenEffect from "./OldScreen";
+
+const ScreenEffect: React.FC = () => (
+  <div className="w-full h-full absolute z-50 pointer-events-none">
+    <OldScreenEffect />
+  </div>
+);
+
+export default ScreenEffect;
