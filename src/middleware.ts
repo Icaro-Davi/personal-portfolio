62f4ad80cfbd1d2efa85f4e633150750c918a650
Icaro Davi <icarodaviduarte@gmail.com>
2024-06-19 23:28:09 -0300

    change: set locale cookie

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import i18nMiddleware from "./middleware/i18n";
+import { CookieKey } from "./utils/cookies/keys";
+import { locales } from "./settings/i18n";
 
 export default async function middleware(req: NextRequest) {
-  const response = await i18nMiddleware(req);
-  const headers = new Headers(response.headers);
+  let headers;
+
+  if (new RegExp("/(?=pt|en).*").test(req.url)) {
+    const i18nResponse = await i18nMiddleware(req);
+    headers = new Headers(i18nResponse.headers);
+  } else {
+    if (!req.cookies.get(CookieKey.LOCALE)) {
+      const defaultLocale = "pt";
+      const acceptLanguage = req.headers.get("accept-language")?.split(";")[0];
+      const locale = acceptLanguage
+        ? locales.find((locale) => acceptLanguage.includes(locale)) ??
+          defaultLocale
+        : defaultLocale;
+      const response = NextResponse.next();
+      response.cookies.set(CookieKey.LOCALE, locale, {
+        sameSite: "strict",
+        httpOnly: true,
+      });
+      headers = new Headers(response.headers);
+    }
+  }
+
   return NextResponse.next({ headers });
 }
-
 export const config = {
-  matcher: [`/((?!_next|favicon.ico)(?=pt|en).*)`],
+  matcher: [`/((?!_next|favicon.ico).*)`],
 };
